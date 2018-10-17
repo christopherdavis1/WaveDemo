634531605b6d7a64961c036ef5eef5ff8180f89e
Christopher Davis <chris@christopheralandavis.com>
2018-10-17 00:59:55 -0400

    Made some changes to the wave to make it oh-so-nersty.

--- flowing1/ViewController.swift
+++ flowing1/ViewController.swift
@@ -12,6 +12,7 @@ import BAFluidView
 class ViewController: UIViewController {
 
     @IBOutlet weak var waveView: UIView!
+    @IBOutlet weak var waveButtonOutlet: UIView!
     
     @IBAction func WaveButton(_ sender: UIButton) {
         if waveHasStarted == false {
@@ -36,6 +37,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.bringSubviewToFront(waveButtonOutlet)
+        
+        waveButtonOutlet.layer.cornerRadius = 40
+        waveButtonOutlet.layer.masksToBounds = true
     }
     
     
@@ -51,29 +56,17 @@ class ViewController: UIViewController {
 
         let wave = BAFluidView(frame: self.view.frame, startElevation: 0.02)!
 
+        wave.strokeColor = UIColor.init(red: 0, green: 0, blue: 0, alpha: 0)
+        wave.fillColor = UIColor.init(red: 0, green: 0, blue: 255, alpha: 0.2)
         wave.maxAmplitude = 4
         wave.minAmplitude = 0
         wave.amplitudeIncrement = 4
-        wave.fillDuration = 30
-        wave.fillRepeatCount = 1
-        wave.fill(to: 0.99)
-        wave.strokeColor = UIColor.init(red: 0, green: 0, blue: 0, alpha: 0)
-        wave.fillColor = UIColor.init(red: 0, green: 0, blue: 255, alpha: 0.2)
-        wave.startAnimation()
+        wave.fillDuration = 10.0
         wave.fillAutoReverse = false
-
-
-//        if startWave == false {
-//            print("Start that wave animation")
-//            wave.fill(to: 0.95)
-//            wave.fillAutoReverse = false
-//            wave.startAnimation()
-//        } else {
-//            print("Stop that wave animation")
-//            wave.stopAnimation()
-//            wave.keepStationary()
-//        }
-
+        wave.fillRepeatCount = 1
+        wave.fill(to: 0.96)
+        wave.startAnimation()
+        
         waveView.addSubview(wave)
 
     }
@@ -82,28 +75,16 @@ class ViewController: UIViewController {
         
         let wave2 = BAFluidView(frame: self.view.frame, startElevation: 0.025)!
         
+        wave2.strokeColor = UIColor.init(red: 0, green: 0, blue: 0, alpha: 0)
+        wave2.fillColor = UIColor.init(red: 0, green: 0, blue: 255, alpha: 0.5)
         wave2.maxAmplitude = 12
         wave2.minAmplitude = 2
         wave2.amplitudeIncrement = 10
-        wave2.fillDuration = 30
+        wave2.fillDuration = 10.0
+        wave2.fillAutoReverse = false
         wave2.fillRepeatCount = 1
         wave2.fill(to: 0.96)
-        wave2.strokeColor = UIColor.init(red: 0, green: 0, blue: 0, alpha: 0)
-        wave2.fillColor = UIColor.init(red: 0, green: 0, blue: 255, alpha: 0.5)
         wave2.startAnimation()
-        wave2.fillAutoReverse = false
-        
-        
-//        if startWave == false {
-//            print("Start that wave animation")
-//            wave2.fill(to: 0.98)
-//            wave2.fillAutoReverse = false
-//            wave2.startAnimation()
-//        } else {
-//            print("Stop that wave animation")
-//            wave2.stopAnimation()
-//            wave2.keepStationary()
-//        }
         
         waveView.addSubview(wave2)
         
@@ -113,16 +94,16 @@ class ViewController: UIViewController {
         
         let wave3 = BAFluidView(frame: self.view.frame, startElevation: 0.018)!
         
+        wave3.strokeColor = UIColor.init(red: 0, green: 0, blue: 0, alpha: 0)
+        wave3.fillColor = UIColor.init(red: 0, green: 0, blue: 255, alpha: 0.15)
         wave3.maxAmplitude = 16
         wave3.minAmplitude = 4
         wave3.amplitudeIncrement = 18
-        wave3.fillDuration = 30
+        wave3.fillDuration = 10.0
+        wave3.fillAutoReverse = false
         wave3.fillRepeatCount = 1
         wave3.fill(to: 0.95)
-        wave3.strokeColor = UIColor.init(red: 0, green: 0, blue: 0, alpha: 0)
-        wave3.fillColor = UIColor.init(red: 0, green: 0, blue: 255, alpha: 0.15)
         wave3.startAnimation()
-        wave3.fillAutoReverse = false
         
         waveView.addSubview(wave3)
         
